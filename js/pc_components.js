eea0bd1fb7ab086d056f61c9b850accd8dee7096
pedro092692 <pedro_092692@hotmail.com>
2024-08-08 22:29:13 -0400

    add js funciton to populate pc parts

---
+++ js/pc_components.js
@@ -0,0 +1,16 @@
+export var pc_parts = {
+
+    cpu:[
+        {
+            name: 'intel 1',
+            price: 100,
+            img_path: '/img/cpu_1.jpg'
+        },
+        {
+            name: 'intel 2',
+            price: 200,
+            img_path: '/img/cpu_25.jpg'
+        },
+    ] 
+        
+}
